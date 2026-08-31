db4c0dfe52a3b07226dda8ba1c996a2321b5d8be
Omar hazem <86628361+omarikas@users.noreply.github.com>
2026-08-31 14:10:48 +0300

    Clean repo: keep only offline PWA directory

--- force-app/main/default/lwc/clmAdminConsole/clmAdminConsole.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class ClmAdminConsole extends LightningElement {
-    @api embedded = false;
-
-    get containerClass() {
-        return this.embedded ? 'clm-admin-console clm-admin-console-embedded' : 'clm-admin-console';
-    }
-}
